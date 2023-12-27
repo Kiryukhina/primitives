daee2d44189117735c8173b87cffbe3313abae4c
Kate <kiryuhinakatya@gmail.com>
2023-12-27 23:10:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 17_543;
+        int bonus = 20;
+        int mili = amount / bonus;
+        int x = mili;
+        System.out.println(x);
+
+
+    }
+}
